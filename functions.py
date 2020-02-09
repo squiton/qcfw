f0bae59a287cc4e38d5fb81f5aafc02e5a66f96d
Stephen Quiton <quiton@usc.edu>
2020-02-09 00:52:46 -0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,114 +1,14 @@
 import numpy as np
 import json,multiprocessing
 
-from ase.calculators.singlepoint import SinglePointCalculator as SPC
-from ase.io import write
-from ase import Atoms
-from ase.constraints import dict2constraint
-from ase.calculators.qchem import QChem
-
 from pymatgen.io.qchem.inputs import QCInput
 from pymatgen.io.qchem.outputs import QCOutput
-from pymatgen.io.ase import AseAtomsAdaptor as PyASE
 from pymatgen.core import Molecule
 
 from custodian.custodian import Custodian
 from custodian.qchem.handlers import QChemErrorHandler
 from custodian.qchem.jobs import QCJob
 
-def atoms_to_encode(images):
-    """ Converts an list of atoms objects to an encoding
-    from a .traj file.
-    """
-
-    if not isinstance(images, list):
-        images = [images]
-
-    # Convert all constraints into dictionary format
-    constraints = [_.todict() for _ in images[0].constraints]
-    for i, C in enumerate(constraints):
-
-        # Turn any arrays in the kwargs into lists
-        for k, v in list(C['kwargs'].items()):
-            if isinstance(v, np.ndarray):
-                constraints[i]['kwargs'][k] = v.tolist()
-
-    # Convert any arrays from the parameter settings into lists
-    keys = images[0].info
-    for k, v in list(keys.items()):
-        if isinstance(v, np.ndarray):
-            keys[k] = v.tolist()
-
-    data = {'trajectory': {}}
-    # Assemble the compressed dictionary of results
-    for i, atoms in enumerate(images):
-
-        if i == 0:
-            # For first images, collect cell and positions normally
-            pos = atoms.get_positions()
-            update_pos = pos
-
-            #cell = atoms.get_cell()
-            #update_cell = cell
-
-            # Add the parameters which do not change
-            data['numbers'] = images[0].get_atomic_numbers().tolist()
-            data['pbc'] = images[0].get_pbc().tolist()
-            data['constraints'] = constraints
-            data['calculator_parameters'] = keys
-
-        else:
-            # For consecutive images, check for duplication
-            # If duplicates are found, do not store it
-            if np.array_equal(atoms.get_positions(), pos):
-                update_pos = np.array([])
-            else:
-                pos = atoms.get_positions()
-                update_pos = pos
-
-            #if np.array_equal(atoms.get_cell(), cell):
-                #update_cell = np.array([])
-            #else:
-                #cell = atoms.get_cell()
-                #update_cell = cell
-
-        if atoms._calc:
-            '''
-            nrg = atoms.get_potential_energy()
-            #force = atoms.get_forces()
-            #stress = atoms.get_stress()
-
-            # Stage results and convert to lists in needed
-            results = {
-            'positions': update_pos,
-            #'cell': update_cell,
-            'energy': nrg,
-            'forces': force,
-            'stress': None}
-            '''
-            #results = {'positions': update_pos}.update(atoms._calc.results)
-            results = atoms._calc.results
-            results['positions'] = update_pos
-        else:
-            results = {
-            'positions': update_pos,
-            #'cell': update_cell
-            }
-
-        for k, v in list(results.items()):
-            if isinstance(v, np.ndarray):
-                results[k] = v.tolist()
-
-        # Store trajectory, throwing out None values
-        data['trajectory'][i] = {
-            k: v for k, v in list(
-                results.items()) if v is not None}
-
-    # Return the reduced results in JSON compression
-    
-    #print(data)
-    #return str(data).replace(''',''')
-    return json.dumps(data)
 def QCOutput_to_encode(qcoutput,more_info=None):
     """ Converts a qcoutput object to an encoding
     from a .out file.
@@ -125,55 +25,6 @@ def QCOutput_to_encode(qcoutput,more_info=None):
     # Return the reduced results in JSON compression
     return json.dumps(data)
     
-def encode_to_atoms(encode):
-    """ Dump the encoding to a local traj file.
-    """
-
-    # First, decode the trajectory
-    data = json.loads(encode, encoding='utf-8')
-
-    # Construct the initial atoms object
-    atoms = Atoms(
-    data['numbers'],
-    data['trajectory']['0']['positions'],
-    #cell=data['trajectory']['0']['cell'],
-    pbc=data['pbc'])
-    atoms.info = data['calculator_parameters']
-    atoms.set_constraint([dict2constraint(_) for _ in data['constraints']])
-
-    # Attach the calculator
-    calc = SPC(
-    atoms=atoms,
-    energy=data['trajectory']['0'].get('energy'),
-    forces=data['trajectory']['0'].get('forces'),
-    stress=data['trajectory']['0'].get('stress'))
-    atoms.set_calculator(calc)
-
-    # Collect the rest of the trajectory information
-    images = [atoms]
-    for i in range(len(data['trajectory']))[1:]:
-        atoms = atoms.copy()
-
-        #if data['trajectory'][str(i)]['cell']:
-            #atoms.set_cell(data['trajectory'][str(i)]['cell'])
-
-        if data['trajectory'][str(i)]['positions']:
-            atoms.set_positions(data['trajectory'][str(i)]['positions'])
-
-        calc = SPC(
-            atoms=atoms,
-            energy=data['trajectory'][str(i)].get('energy'),
-            forces=data['trajectory'][str(i)].get('forces'),
-            stress=data['trajectory'][str(i)].get('stress'))
-        atoms.set_calculator(calc)
-
-        images += [atoms]
-
-    # Write the traj file
-    #if out_file:
-        #write(out_file, images)
-
-    return images[0]
     
 def encode_to_QCInput(encode,rem,pcm=None,solvent=None):
     """Takes final geometry from encode, creates QCInput object
@@ -196,24 +47,6 @@ def encode_to_QCInput(encode,rem,pcm=None,solvent=None):
     NewInput = QCInput(molecule = opt_geom,rem=rem,pcm=pcm,solvent=solvent)
     return NewInput
     
-def get_potential_energy(label,encoding=None,keys=None):
-    #If no encoding provided, assume that input file is already written
-    if encoding != None:
-        #obtain atoms object and QChem keys
-        mol = encode_to_atoms(encoding)
-        #keys = mol.info
-        mol.info = keys
-    #Construct calculator and write input file, use max number of threads
-    calc = QChem(label=label,**keys,nt=multiprocessing.cpu_count())
-    if encoding != None:
-        calc.write_input(atoms = mol, properties = keys)
-        mol.set_calculator(calc)
-        #Perform relaxation
-        mol.get_potential_energy()
-    else:
-        calc.calculate()
-    #Get the output encoding
-    return parse_out(label=label,keys=keys)
 
 def run_QChem(label,encode=None,rem=None,pcm=None,solvent=None):
     inname = label + '.inp'
@@ -328,19 +161,6 @@ def run_QChem_EDA(label,rem,encode=None,pcm=None,solvent=None):
     c.run()
     return parse_out_QChem(label=label,keys=keys)
 
-#Turns output into encoding
-def parse_out(label,keys):
-    output = QCOutput(filename=label+'.out')
-    if keys['jobtype']=='opt':
-        mol=PyASE.get_atoms(output.data['molecule_from_last_geometry'],info=keys)
-    else:
-        mol=PyASE.get_atoms(output.data['initial_molecule'],info=keys)
-    #if calc == None:
-    calc = QChem(label=label,**keys)
-    calc.read_results()
-    mol.set_calculator(calc)
-    encoding = atoms_to_encode(mol)
-    return encoding
 #Turns output into encoding
 def parse_out_QChem(label):
     output = QCOutput(filename=label+'.out')
